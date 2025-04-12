f5a3d90372d2c2025b6e8a682f88f7b7d9b6d2cd
HariOm <hariomspanchal98@gmail.com>
2025-04-12 13:56:47 +0530

    added reusable components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatIconModule} from '@angular/material/icon';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { UpdateUserComponent } from './update-user/update-user.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FooterComponent } from './footer/footer.component';
 import { FormsModule } from '@angular/forms';
 import { DeleteUserComponent } from './delete-user/delete-user.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { VendorChartComponent } from './vendor-chart/vendor-chart.component';
+import { UserTableComponent } from './user-table/user-table.component';
 
 @NgModule({
   declarations: [
@@ -20,12 +22,14 @@ import { DeleteUserComponent } from './delete-user/delete-user.component';
     SidebarComponent,
     UpdateUserComponent,
     FooterComponent,
-    DeleteUserComponent
+    DeleteUserComponent,
+    NavbarComponent,
+    VendorChartComponent,
+    UserTableComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatIconModule,
     BrowserAnimationsModule,
     HttpClientModule,
     FormsModule
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -7,69 +7,7 @@ import { DashboardService } from '../dashboard.service';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  showUser = false;
-  deleteUserPopup = false;
-  selectedUser: any;
-  users: any = [];
-  columnHeaders: any = [];
-  masterSelected: boolean = false;
+  constructor() {}
 
-  constructor(private dashboardService: DashboardService) { }
-
-  ngOnInit(): void {
-    this.getUsers();
-  }
-
-  getUsers() {
-    this.dashboardService.getUsers().subscribe((data: any) => {
-      this.users = data['grid_data'];
-      this.columnHeaders = data['grid_columns'];
-      this.getUserProfile();
-      console.log(this.users[0]);
-    });
-  }
-
-  getUserProfile() {
-    this.users.forEach((user: any) => {
-      user['profileUrl'] = 'https://avatar.iran.liara.run/public/' + user.id.replace(/\D/g, '').slice(-2);
-      user['selected'] = false;
-    });
-  }
-
-  checkIfAllSelected() {
-    this.masterSelected = this.users.every((user: any) => user.selected);
-  }
-
-  selectAllRows() {
-    this.users.forEach((user: any) => {
-      user.selected = this.masterSelected;
-    });
-  }
-
-  openUserPopup(user: any) {
-    this.selectedUser = user;
-    this.showUser = true;
-  }
-
-  closeUserPopup() {
-    this.showUser = false;
-    this.selectedUser = null;
-  }
-
-  openDeletePopup(user: any) {
-    this.selectedUser = user;
-    this.deleteUserPopup = true;
-  }
-
-  closeDeletePopup() {
-    this.deleteUserPopup = false;
-    this.selectedUser = null;
-    console.log('after', this.selectedUser);
-  }
-
-  deleteUser() {
-    this.users = this.users.filter((user: any) => user.id !== this.selectedUser.id);
-    console.log('before', this.selectedUser);
-    this.closeDeletePopup();
-  }
+  ngOnInit(): void {}
 }
